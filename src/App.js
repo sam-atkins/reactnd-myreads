064d217937a9819812061fcc9313cbc84befbc7b
Sam Atkins <samatkins5@gmail.com>
2017-10-29 08:55:44 +0000

    refactor: Move statement mgmt to App
    - remove BookLibrary component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 // import * as BooksAPI from './utils/BooksAPI';
 import './App.css';
 import { Header, Footer } from './components/common';
-import { BookLibrary, SearchPage } from './components';
+import { BookShelf, SearchPage } from './components';
 
-class BooksApp extends React.Component {
-  state = {};
+class BooksApp extends Component {
+  state = {
+    shelves: [
+      {
+        id: 0,
+        shelfName: 'Currently Reading',
+      },
+      {
+        id: 1,
+        shelfName: 'Want to Read',
+      },
+      {
+        id: 2,
+        shelfName: 'Read',
+      },
+    ],
+  };
 
   render() {
     return (
       <div className="app">
         <Header />
-        <Route exact path="/" component={BookLibrary} />
+        <Route
+          exact
+          path="/"
+          render={() => <BookShelf shelves={this.state.shelves} />}
+        />
         <Route
           path="/search"
           render={({ history }) => (
