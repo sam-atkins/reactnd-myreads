9079a3c683e9f9b70ba99b46467487319fc2f9af
Sam Atkins <samatkins5@gmail.com>
2017-10-29 08:56:40 +0000

    fix: Remove export

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
 export { default as Book } from './Book';
-export { default as BookLibrary } from './BookLibrary';
 export { default as BookShelf } from './BookShelf';
 export { default as SearchPage } from './SearchPage';
