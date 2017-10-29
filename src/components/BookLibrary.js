ec68e3760c5d4cec3e68d5dd22d8e21adb36bb50
Sam Atkins <samatkins5@gmail.com>
2017-10-29 06:57:25 +0000

    feat: Extract out further html tags into components

---
+++ src/components/BookLibrary.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import BookShelf from './BookShelf';
+
+class BookLibrary extends Component {
+  state = {  }
+  render() {
+    return (
+      <BookShelf />
+    );
+  }
+}
+
+export default BookLibrary;
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Book } from './Book';
+export { default as BookLibrary } from './BookLibrary';
+export { default as BookShelf } from './BookShelf';
+export { default as SearchPage } from './SearchPage';
