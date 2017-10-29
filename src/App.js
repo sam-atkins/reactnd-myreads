f98950be764f110c02bdf593a530ed1609e43cbc
Sam Atkins <samatkins5@gmail.com>
2017-10-29 08:38:26 +0000

    feat: Add React Router
    - navigate to home and search pages
    - with URL
    - back button in browser returns to home page

--- src/App.js
+++ src/App.js
@@ -1,36 +1,28 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 // import * as BooksAPI from './utils/BooksAPI';
 import './App.css';
 import { Header, Footer } from './components/common';
 import { BookLibrary, SearchPage } from './components';
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false,
-  };
+  state = {};
 
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <SearchPage />
-        ) : (
-          <div className="list-books">
-            <Header />
-            <BookLibrary />
-            <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>
-                Add a book
-              </a>
-            </div>
-          </div>
-        )}
+        <Header />
+        <Route exact path="/" component={BookLibrary} />
+        <Route
+          path="/search"
+          render={({ history }) => (
+            <SearchPage
+              onAddBook={() => {
+                history.push('/');
+              }}
+            />
+          )}
+        />
         <Footer />
       </div>
     );
--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Book from './Book';
 
 const BookShelf = props => (
   <div className="list-books-content">
     {props.shelves.map(shelf => (
-      <div className="bookshelf">
+      <div className="bookshelf" key={shelf.id}>
         <h2 className="bookshelf-title">{shelf.shelfName}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
@@ -14,11 +15,16 @@ const BookShelf = props => (
         </div>
       </div>
     ))}
+    <div className="open-search">
+      <Link to="/search">
+        Add a book
+      </Link>
+    </div>
   </div>
 );
 
 BookShelf.propTypes = {
-  shelves: PropTypes.shape.isRequired,
+  shelves: PropTypes.array.isRequired,
 };
 
 export default BookShelf;
