24c5a903246cc894f3afb0a4c47222a9f3c2b934
Sam Atkins <samatkins5@gmail.com>
2017-10-31 10:47:26 +0000

    test: Add snapshot tests for Header and Footer components

---
+++ src/components/common/Footer.test.js
@@ -0,0 +1,14 @@
+/* global describe, it, expect */
+
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Footer from './Footer';
+
+describe('Footer', () => {
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(<Footer />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
---
+++ src/components/common/Header.test.js
@@ -0,0 +1,12 @@
+/* global describe, it, expect */
+
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Header from './Header';
+
+describe('Header', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(<Header />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
