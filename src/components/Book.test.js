3d0107fd1e197d2e688f563b5156a491a61a70af
Sam Atkins <samatkins5@gmail.com>
2017-11-06 14:33:18 +0000

    test: Update Book component snapshot

--- src/components/Book.test.js
+++ src/components/Book.test.js
@@ -4,9 +4,18 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Book from './Book';
 
+const book = {
+  title: 'A Book',
+  authors: ['Author A'],
+  imageLinks: { smallThumbnail: 'someUrl' },
+  shelf: 'wantToRead',
+};
+
 describe('Book', () => {
   it('renders correctly', () => {
-    const tree = renderer.create(<Book />).toJSON();
+    const tree = renderer
+      .create(<Book onUpdateBook={() => {}} book={book} />)
+      .toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
