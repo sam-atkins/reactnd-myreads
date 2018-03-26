cf995d9bcb16c47b0f4ea99a13b7f21e09de8741
Sam Atkins <samatkins5@gmail.com>
2018-03-26 15:37:34 +0100

    refactor: convert to tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+const FooterWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 2vh;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+`;
+
+const FooterText = styled.div`
+  font-size: 0.8em;
+`;
+
+const Footer = () => (
+  <FooterWrapper>
+    <FooterText>
+      Built with ♥ by <a href="https://github.com/cubiio">Cubiio</a>. View
+      source code on{' '}
+      <a href="https://github.com/cubiio/reactnd-myreads">Github</a>.
+    </FooterText>
+  </FooterWrapper>
+);
+
+export default Footer;
